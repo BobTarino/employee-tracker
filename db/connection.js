f2c7e104b63fe25d7330634a1c56dbcc05973130
Bobby T <bob.tarino@gmail.com>
2021-06-06 20:13:41 -0500

    created tables in mysql and added queries to database

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
     user: 'root',
     // Your MySQL password
     password: 'ClaudiaAndre327',
-    database: 'election'
+    database: ''
 });
 
 module.exports = db;
